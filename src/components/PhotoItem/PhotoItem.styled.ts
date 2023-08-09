c532a24f425fad89ca1e22480a9a06206d57d6b6
Martin Bobbio <martinbobbio1@gmail.com>
2023-08-09 17:31:34 -0300

    update: style in photo

--- src/components/PhotoItem/PhotoItem.styled.ts
+++ src/components/PhotoItem/PhotoItem.styled.ts
@@ -7,6 +7,7 @@ import { styled } from 'styled-components';
 export const PhotoItemStyled = styled.img`
   transition: opacity 0.3s ease;
   height: 100%;
+  width: 100%;
   &:hover {
     opacity: 0.7;
     cursor: pointer;
