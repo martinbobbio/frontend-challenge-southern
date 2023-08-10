7a8d470653e364c943019d89875d5ab50e2608ec
Martin Bobbio <martinbobbio1@gmail.com>
2023-08-09 22:33:49 -0300

    update: assets routes

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,24 @@
-// Branding
-export { default as logoNASA } from './images/branding/NASA.svg';
-export { default as logoMBLight } from './images/branding/logo-mb-light.svg';
-export { default as logoMBDark } from './images/branding/logo-mb-dark.svg';
-// Others
-export { default as curiosityDefault } from './images/other/curiosity-default.jpg';
-export { default as opportunityDefault } from './images/other/opportunity-default.jpg';
-export { default as spiritDefault } from './images/other/spirit-default.jpg';
+import logoNASA from './images/branding/NASA.svg';
+import logoMBLight from './images/branding/logo-mb-light.svg';
+import logoMBDark from './images/branding/logo-mb-dark.svg';
+import curiosityDefault from './images/other/curiosity-default.jpg';
+import opportunityDefault from './images/other/opportunity-default.jpg';
+import spiritDefault from './images/other/spirit-default.jpg';
+
+export {
+  logoNASA,
+  logoMBLight,
+  logoMBDark,
+  curiosityDefault,
+  opportunityDefault,
+  spiritDefault,
+};
+
+// // Branding
+// export { default as logoNASA } from './images/branding/NASA.svg';
+// export { default as logoMBLight } from './images/branding/logo-mb-light.svg';
+// export { default as logoMBDark } from './images/branding/logo-mb-dark.svg';
+// // Others
+// export { default as curiosityDefault } from './images/other/curiosity-default.jpg';
+// export { default as opportunityDefault } from './images/other/opportunity-default.jpg';
+// export { default as spiritDefault } from './images/other/spirit-default.jpg';
